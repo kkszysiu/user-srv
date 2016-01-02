2240a4a6a7578f67e4d24bcec1ea6d4ccf603ffd
Asim <asim@chuhnk.me>
2016-01-02 20:54:19 +0000

    Use service definition

--- main.go
+++ main.go
@@ -1,39 +1,38 @@
 package main
 
 import (
-	"github.com/codegangsta/cli"
 	log "github.com/golang/glog"
-	"github.com/micro/go-micro/cmd"
-	"github.com/micro/go-micro/server"
+	"github.com/micro/cli"
+	"github.com/micro/go-micro"
 	"github.com/micro/user-srv/db"
 	"github.com/micro/user-srv/handler"
 	proto "github.com/micro/user-srv/proto/account"
 )
 
 func main() {
-	cmd.Flags = append(cmd.Flags,
-		cli.StringFlag{
-			Name:   "database_url",
-			EnvVar: "DATABASE_URL",
-			Usage:  "The database URL e.g root@tcp(127.0.0.1:3306)/user",
-		},
+	service := micro.NewService(
+		micro.Name("go.micro.srv.user"),
+		micro.Flags(
+			cli.StringFlag{
+				Name:   "database_url",
+				EnvVar: "DATABASE_URL",
+				Usage:  "The database URL e.g root@tcp(127.0.0.1:3306)/user",
+			},
+		),
+
+		micro.Action(func(c *cli.Context) {
+			if len(c.String("database_url")) > 0 {
+				db.Url = c.String("database_url")
+			}
+		}),
 	)
 
-	cmd.Actions = append(cmd.Actions, func(c *cli.Context) {
-		db.Url = c.String("database_url")
-	})
-
-	cmd.Init()
-
+	service.Init()
 	db.Init()
 
-	server.Init(
-		server.Name("go.micro.srv.user"),
-	)
+	proto.RegisterAccountHandler(service.Server(), new(handler.Account))
 
-	proto.RegisterAccountHandler(server.DefaultServer, new(handler.Account))
-
-	if err := server.Run(); err != nil {
+	if err := service.Run(); err != nil {
 		log.Fatal(err)
 	}
 }
